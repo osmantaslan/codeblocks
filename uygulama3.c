35bedb98483816505b88a2cfd5ed3624591d562b
Osman <117210126+osmantaslan@users.noreply.github.com>
2022-11-02 13:58:43 +0300

    Create uygulama3.c

---
+++ uygulama3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+    int a,b,c;
+
+    a=5;
+    b=-6;
+
+
+    c=a+b;
+
+    printf("a:%d b:%d c:%d\n",a,b,c);
+
+    a=10;
+
+    c=a+b;
+
+    printf("a:%d b:%d c:%d",a,b,c);
+
+
+
+
+    return 0;
+}
