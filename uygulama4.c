dab9b42d89e75af463eb0d65b7ab6ac55d79fd6d
Osman <117210126+osmantaslan@users.noreply.github.com>
2022-11-02 13:58:49 +0300

    Create uygulama4.c

---
+++ uygulama4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+
+
+/*
+
+sqrt() -> karekok  float
+abs() -> tam sayý için mutlak deger
+fabs()-> ondalýklý sayý için mutlak deðer
+pow() -> us alma
+sin() -> sinus
+cos() -> cos
+tan() -> tajant
+exp() -> exp hesaplar  (double)
+
+*/
+
+int main()
+{
+    float aci,sonuc;
+
+    aci=M_PI/6;
+
+    sonuc=tan(aci);
+
+    printf("Sonuc:%f",sonuc);
+
+
+    return 0;
+}
