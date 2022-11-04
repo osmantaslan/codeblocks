64ba137c04085e7b6d497e66bd01642fbd050c9a
Osman <117210126+osmantaslan@users.noreply.github.com>
2022-11-04 17:51:58 +0300

    Create uygulama5.c

---
+++ uygulama5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define esik1 5.5
+
+const float esik2=8.8;
+
+int main()
+{
+   float deger=4;
+
+   printf("Karsilastima Sonucu:%d",deger>=esik1 && deger<esik2 );
+
+
+    return 0;
+}
